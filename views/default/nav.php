4f0d7d48f42e16e63f705e96fb23c74b02186537
Darlan <darlan.sgf@gmail.com>
2019-07-04 11:41:47 -0300

    add logo

--- views/default/nav.php
+++ views/default/nav.php
@@ -1,11 +1,21 @@
-<nav class="nav-extended" style="background: rgba(22, 160, 133,.8); height: 7em;">
+<style>
+#menu {
+  position: fixed;
+}
+</style>
+<nav class="nav-extended" id="menu" style="background: rgba(22, 160, 133,.8); height: 7em;">
     <div class="nav-wrapper container">
-        <a href="#" class="brand-logo"><img src="Public/img/LogoIHM.png"></a>
+        <a href="#" class="brand-logo" style="margin-top:-15px;"><img src="Public/img/LogoIHC1.png"></a>
         <div class="right">
-            <ul>
+            <ul class="tabs-transparent" style="margin-top: 2em;">
                 <li class="tab"><a href="views/home.php">Cidadão</a></li>
                 <li class="tab"><a href="views/funcionario.php">Funcionário</a></li>
             </ul>
         </div>
     </div>
 </nav>
+<br>
+<br>
+<br>
+<br>
+<br>
